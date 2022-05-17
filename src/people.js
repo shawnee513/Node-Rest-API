e12c6fec23b73ea77b8b66f4a2b5b86b9c1d9545
shawnee513 <shanpan513@gmail.com>
2022-05-17 13:22:46 -0600

    Finished tutorial, final commit

---
+++ src/people.js
@@ -0,0 +1,13 @@
+export let people = [{
+    name: "John", 
+    age: 35,
+    airColor: "brown",
+}, {
+    name: "Joe",
+    age: 20,
+    hairColor: "blonde",
+}, {
+    name: "Jane",
+    age: 40,
+    haircolor: "red",
+}];
